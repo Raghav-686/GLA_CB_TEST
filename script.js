da12f9be54a85c0915adb6eb28b51acfc0f2f0cb
Raghav-686 <92645959+Raghav-686@users.noreply.github.com>
2022-12-07 09:37:53 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,42 @@
+const text = document.getElementById('text');
+const container = document.getElementById('add');
+const button = document.getElementById('button');
+var str=``;
+var flag = false;
+var id=1;
+function add()
+{
+    container.style.display = "inline-block";
+    button.style.display = "";
+}
+
+function addContent()
+{
+    container.style.display = "none";
+    button.style.display = "none";
+    console.log(document.getElementById('text').value);
+    str+=`<div id="inner">
+            <div class="delete"></div>
+    <textarea name="" id="cont" cols="36" rows="12">${document.getElementById('text').value}</textarea>
+        </div>`;
+        document.getElementById('text').value="";
+    document.getElementById('add-container').innerHTML = str;    
+}
+
+function deleteData()
+{
+     flag = true;
+     console.log("gbrejbhgerj");
+}
+
+document.getElementById('inner').addEventListener("click", myFunction);
+
+function myFunction()
+{
+    console.log("cjdshjvghjrgh")
+    if(flag == true)
+    {
+        document.getElementById('inner').style.display = "none";
+    }
+    // flag = false;
+}
